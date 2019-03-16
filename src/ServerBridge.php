56f64e95f1804b1c0c94a2b3411d22c8be0a72e4
Michael Reed <villascape@gmail.com>
2019-03-16 12:39:27 +0000

    EOL changes

--- src/ServerBridge.php
+++ src/ServerBridge.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Greenbean\ServerBridge;
 class ServerBridge
 {
--- src/ServerBridgeUncatchableException.php
+++ src/ServerBridgeUncatchableException.php
@@ -1,3 +1,4 @@
 <?php
+
 namespace Greenbean\ServerBridge;
 class ServerBridgeUncatchableException extends \Exception{}
