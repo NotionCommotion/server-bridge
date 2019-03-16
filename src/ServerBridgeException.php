99795ae46289247ee4873c6727a3f8c3a0f9d06e
Michael Reed <villascape@gmail.com>
2019-03-16 12:32:47 +0000

    changed EOL and minor change

--- src/ServerBridgeException.php
+++ src/ServerBridgeException.php
@@ -4,14 +4,11 @@ class ServerBridgeException extends \Exception
 {
     //code is http status code.  code and previous exception are required
     private $json=false;
-    public function __construct($body, int $code, \Exception $previous) {
+    public function __construct($message, int $code, \Exception $previous) {
         if(!is_string($message)) {
             $this->json=$message;
             $message='JSON error message';
         }
         parent::__construct($message, $code, $previous);
-    }
-    public function getMessage() {
-        return $this->json?$this->json:parent::getMessage();
     }
 }
